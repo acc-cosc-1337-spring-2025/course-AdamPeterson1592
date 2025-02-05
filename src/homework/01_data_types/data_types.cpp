f25022d2d8d6c2bb01553387881f91401c61bbe6
AdamPeterson1592 <adam.peterson2@g.austincc.edu>
2025-02-05 23:59:19 +0000

    Added Multiplication functionality and tests

--- src/homework/01_data_types/data_types.cpp
+++ src/homework/01_data_types/data_types.cpp
@@ -5,6 +5,10 @@ int add_numbers(int num1, int num2)
 {
 	return num1 + num2;
 }
+int multiply_numbers(int num1){
+	int num2 = 5;
+	return num1 * num2;
+}
 
 //write function code here
 
--- src/homework/01_data_types/main.cpp
+++ src/homework/01_data_types/main.cpp
@@ -1,12 +1,16 @@
 ﻿//write include statements
+#include "data_types.h"
+#include <iostream>
 
 
 //write namespace using statement for cout
-
+using namespace std;
 
 int main()
 {
-	
+	int num = 0;
+
+	cout<<num<<"\n";
 
 	return 0;
 }
