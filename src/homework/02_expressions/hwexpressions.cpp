ce1c9b5a1d1b2ba2d1f09044aba0a0359b870742
AdamPeterson1592 <adam.peterson2@g.austincc.edu>
2025-02-06 00:17:23 +0000

    Added sales tax function and prototype.

--- src/homework/02_expressions/hwexpressions.cpp
+++ src/homework/02_expressions/hwexpressions.cpp
@@ -5,6 +5,10 @@ int add_numbers(int num1, int num2)
 {
 	return num1 + num2;
 }
+double get_sales_tax_amount(double meal_amount){
+	double tax_rate = 0.825;
+	return tax_rate * meal_amount;
+}
 
 //write function code here
 
--- src/homework/02_expressions/hwexpressions.h
+++ src/homework/02_expressions/hwexpressions.h
@@ -2,6 +2,7 @@
 int add_numbers(int num1, int num2);
 
 //write function prototype here
+double get_sales_tax_amount(double meal_amount);
 
 
 
