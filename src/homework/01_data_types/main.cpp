10f37bbfcb61dffe459e7bdec2a8e3d126b254bb
AdamPeterson1592 <adam.peterson2@g.austincc.edu>
2025-02-06 00:10:47 +0000

    Added main functioanlity of input and multiplications.

--- src/homework/01_data_types/main.cpp
+++ src/homework/01_data_types/main.cpp
@@ -8,9 +8,21 @@ using namespace std;
 
 int main()
 {
+	//initialize num, num1, result
 	int num = 0;
+	int num1 = 4;
+	int result = 0;
 
-	cout<<num<<"\n";
+	//get input
+	cout << "Number:";
+	cin >> num;
+
+	//output result of 5 * input and 5 * 4
+	result = multiply_numbers(num);
+	cout << result << "\n";
+
+	result = multiply_numbers(num1);
+	cout << result << "\n";
 
 	return 0;
 }
