93fb570a4a74c4972d76afcd5a983f17278ad488
AdamPeterson1592 <adam.peterson2@g.austincc.edu>
2025-02-06 00:19:37 +0000

    Added tip function and prototype.

--- src/homework/02_expressions/hwexpressions.cpp
+++ src/homework/02_expressions/hwexpressions.cpp
@@ -5,11 +5,14 @@ int add_numbers(int num1, int num2)
 {
 	return num1 + num2;
 }
+
 double get_sales_tax_amount(double meal_amount){
 	double tax_rate = 0.825;
 	return tax_rate * meal_amount;
 }
-
+double get_tip_amount(double meal_amount, double tip_rate){
+	return meal_amount * tip_rate;
+}
 //write function code here
 
 
