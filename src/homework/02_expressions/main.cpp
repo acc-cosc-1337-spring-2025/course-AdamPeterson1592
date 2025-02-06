3785e94aa1b37d9b9a1f0563ddbc882e5ecf99a4
AdamPeterson1592 <adam.peterson2@g.austincc.edu>
2025-02-06 00:51:43 +0000

    Added recipt calculator to main.

--- src/homework/02_expressions/main.cpp
+++ src/homework/02_expressions/main.cpp
@@ -1,10 +1,32 @@
 ﻿//write include statements
-
+#include <iostream>
+#include "hwexpressions.h"
 
 //write namespace using statement for cout
-
+using namespace std;
 int main()
 {
+	double meal_amount, tip_rate, tip_amount, tax_amount, total;
+
+	cout << "Meal cost: ";
+	cin >> meal_amount;
+	
+	cout << "Tip rate: ";
+	cin >> tip_rate;
+
+	cout << "\n";
+
+	tax_amount = get_sales_tax_amount(meal_amount);	
+
+	tip_amount = get_tip_amount(meal_amount, tip_rate);
+
+	total = meal_amount + tax_amount + tip_amount;
+	
+	//should have used printf and ($.2f, val)
+	cout << "Amount: " << meal_amount << "\n"
+		 << "Tax: " << tax_amount << "\n"
+		 << "Tip: " << tip_amount << "\n"
+		 << "Total: " << total << "\n";
 	
 
 	return 0;
